499e48954b30b736ce0523478d629e98ef401435
janhavi07 <56464138+janhavi07@users.noreply.github.com>
2019-11-03 18:30:53 +0530

    Update Anagram.java

--- AlogarithmPrograms/Anagram.java
+++ AlogarithmPrograms/Anagram.java
@@ -1,29 +1,55 @@
 package AlogarithmPrograms;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import Utility.*;
-public class Anagram
+public class anagram
 {
-	
+	static int count=0;
+	public static void anagram(String s1,String s2) 
+	{
+		if(s1.length()!=s2.length())
+		{
+			System.out.println("NOT AN ANAGRAM");
+		}
+		else
+		{
+			for(int i=0;i<s1.length();i++)
+			{
+				for(int j=0;j<s2.length();j++)
+				{
+					
+					{
+						if(s1.charAt(i)==s2.charAt(j))
+						{
+						  count++;	
+						}
+						
+					}
+				}
+			}
+			
+			
+		}
+		if(count==s1.length())
+		{
+			System.out.println("it is an anagram");
+		}
+		else
+		{
+			System.out.println("not an anagram");
+		}
+		
+	}
 	public static void main(String[] args) 
 	{
-		Anagram anagram=new Anagram();
-		System.out.println("enter string 1");
-		String s1=Utility.readString();
-		System.out.println("enter string 2");
-		String s2=Utility.readString();
-	    char[] charArray1=Utility.convertToLowerCase(s1);
-		char[] charArray2=Utility.convertToLowerCase(s2);
-		if(Utility.anagramLogic(charArray1,charArray2)) 
-			System.out.println("it is a anagram");
-		else
-		    System.out.println("not an anagram");
+		// TODO Auto-generated method stub
+		String s1="heart";
+		String s2="earth";
+		anagram(s1,s2);
 		
 		
-		
-		
-
-	}
+				}
 
 }
+
+
+
+	
